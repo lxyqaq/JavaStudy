594f1a244f328f7588f58e49e9318390e90c817a
lxyqaq <lxyqaq0128@gmail.com>
2020-09-04 13:47:29 +0100

    Java流程控制语句(if-else的练习)

---
+++ day03_Java流程控制语句/src/IfExercise.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * @author lxyqaq
+ * @date 2020/9/4 - 13:15
+ */
+public class IfExercise {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 1;
+        if (x > 2)
+            if (y > 2)
+                System.out.println(x + y);
+                //System.out.println("atguigu");
+            else //就近原则
+                System.out.println("x is " + x);
+
+
+        //课后练习3：测算狗的年龄
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入狗的年龄：");
+        int age = scan.nextInt();
+        if (age >= 0 && age <= 2) {
+            System.out.println("狗的年龄为：" + age * 2);
+        } else if (age < 0) {
+            System.out.println("您输入的年龄有误");
+        } else {
+            System.out.println("狗的年龄为：" + (21 + 4 * (age - 2)));
+        }
+
+
+
+    }
+}
