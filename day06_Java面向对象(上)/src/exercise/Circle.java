92c702e9dfe946cce5b3935f86540c5664975b64
lxyqaq <lxyqaq0128@gmail.com>
2020-09-08 17:39:56 +0100

    Circle类

---
+++ day06_Java面向对象(上)/src/exercise/Circle.java
@@ -0,0 +1,15 @@
+package exercise;
+
+/**
+ * @author lxyqaq
+ * @date 2020/9/8 - 17:20
+ */
+public class Circle {
+    public double radius;
+
+
+    public double findArea() {
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+}
