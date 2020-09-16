1cc8b48b2a105045a45f466e8c3265fe2ae657f7
lxyqaq <lxyqaq0128@gmail.com>
2020-09-16 23:02:57 +0100

    equals方法的练习2

---
+++ day10_Java面向对象(中)/src/exercise4/MyDateTest.java
@@ -0,0 +1,77 @@
+package exercise4;
+
+import java.util.Objects;
+
+/**
+ * @ClassName MyDateTest
+ * @Description equals方法的练习
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2020/9/16 22:59
+ * @Version 1.0
+ */
+public class MyDateTest {
+    public static void main(String[] args) {
+        MyDate m1 = new MyDate(14, 3, 1976);
+        MyDate m2 = new MyDate(14, 3, 1976);
+        if (m1 == m2) {
+            System.out.println("m1==m2");
+        } else {
+            System.out.println("m1!=m2"); // m1 != m2
+        }
+
+        if (m1.equals(m2)) {
+            System.out.println("m1 is equal to m2");// m1 is equal to m2
+        } else {
+            System.out.println("m1 is not equal to m2");
+        }
+    }
+}
+
+class MyDate{
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    //重写equals方法
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyDate myDate = (MyDate) o;
+        return day == myDate.day &&
+                month == myDate.month &&
+                year == myDate.year;
+    }
+
+}
