7e90471787b05a3df15bb09178f6eaec38a6b9a0
lxyqaq <lxyqaq0128@gmail.com>
2020-09-17 13:50:05 +0100

    Java面向对象设计⑤

---
+++ day10_Java面向对象(中)/src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName GitTest
+ * @Description Git的注解
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2020/9/17 13:49
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+    }
+}
