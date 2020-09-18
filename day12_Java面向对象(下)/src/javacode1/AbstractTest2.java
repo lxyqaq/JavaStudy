f6458dafeffd50f36be94dc547ab75154fbc90b9
lxyqaq <lxyqaq0128@gmail.com>
2020-09-18 22:06:13 +0100

    abstract关键字的注意

---
+++ day12_Java面向对象(下)/src/javacode1/AbstractTest2.java
@@ -0,0 +1,20 @@
+package javacode1;
+
+/**
+ * @ClassName AbstractTest2
+ * @Description abstract关键字的注意事项
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2020/9/18 22:05
+ * @Version 1.0
+ */
+public class AbstractTest2 {
+    public static void main(String[] args) {
+        /*
+         * abstract使用上的注意点：
+         * 1.abstract不能用来修饰：属性、构造器等结构
+         *
+         * 2.abstract不能用来修饰私有方法、静态方法、final的方法、final的类
+         *
+         */
+    }
+}
