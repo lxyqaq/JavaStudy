ccc7d0c5e970e248349839ffe82d9688ec14028d
Xiangyu Liu <lxyqaq0128@gmail.com>
2020-12-07 15:25:26 +0000

    Assignment

--- Assignment_3/src/main/Main.java
+++ Assignment_3/src/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static TopJFrame top;
 
     public static void main(String[] args) throws IOException {
-        top = new TopJFrame("Staff Information Management System");
+        top = new TopJFrame("Employee Information Management System");
     }
 
 }
